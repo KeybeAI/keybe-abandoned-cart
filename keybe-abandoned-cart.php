c0318368102baf1cee451e7db98e995b56f5f9b9
TodoViernes <santiagovallejoc@gmail.com>
2023-01-25 21:07:05 -0600

    fix code and variables

--- keybe-abandoned-cart.php
+++ keybe-abandoned-cart.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: Keybe Abandoned Cart
  * Plugin URI: https://keybe.ai/
@@ -17,61 +16,64 @@ defined('ABSPATH') || exit;
 
 include 'includes/settings-page.php';
 
-add_filter('woocommerce_webhook_deliver_async', '__return_false');
-
 //  Script to capture data from abandoned cart
-
 add_action('woocommerce_after_checkout_form', 'keybe_abandoned_cart_script');
 
-function keybe_abandoned_cart_script()
-{
+function keybe_abandoned_cart_script(){
 	// Get settings from settings page
 	$options = get_option('keybe_settings');
-	// C
-	if ($options['keybe_app_id'] && $options['keybe_api_key'] && $options['keybe_company_id'] && $options['keybe_country_code']){
+	$keybe_app_id = $options['keybe_app_id'];
+	$keybe_company_id = $options['keybe_company_id'];
+	$keybe_api_key = $options['keybe_api_key'];
+	$keybe_country_code = $options['keybe_country_code'];
+	if ($keybe_app_id && $keybe_company_id && $keybe_api_key && $keybe_country_code){
 		$active = 1;
 	} else {
 		$active = 0;
 	}
-?>
-	<?php if ($active === 1) : ?>
-		<script>
-			console.log('KeyBe Abandoned Cart Ready!! 🚀');
-			jQuery('form[name="checkout"]').change((item) => {
-				if (["billing_phone", "billing_email"].includes(item.target.name)) {
-					var values = jQuery('form[name="checkout"]').serializeArray();
-					console.log(values);
-					let country_code = '<?php echo $options['keybe_country_code']; ?>';
-					fetch('https://wrzy3jtldi.execute-api.us-east-1.amazonaws.com/prod/woocommerce/generate-abandoned-cart', {
-						method: 'POST',
-						headers: new Headers({
-							'Content-Type': 'application/json'
-						}),
-						body: JSON.stringify({
-							userData: {
-								name: values.find((item) => item.name === 'billing_first_name')?.value,
-								lastName: values.find((item) => item.name === 'billing_last_name')?.value,
-								phone: country_code + values.find((item) => item.name === 'billing_phone')?.value,
-								email: values.find((item) => item.name === 'billing_email')?.value,
-							},
-							keybeClientData: {
-								companyUUID: '<?php echo $options['keybe_company_id']; ?>',
-								appUUID: '<?php echo $options['keybe_app_id']; ?>',
-								publicKey: '<?php echo $options['keybe_api_key']; ?>',
-								haveSearchPhoneUser: true,
-							},
-						}),
-					});
-				}
-			})
-		</script>
-	<?php endif; ?>
-<?php
+
+	if ($active === 1) : ?>
+			<script>
+				console.log('KeyBe Abandoned Cart Ready!! 🚀');
+				jQuery('form[name="checkout"]').change((item) => {
+					if (["billing_phone", "billing_email"].includes(item.target.name)) {
+						var values = jQuery('form[name="checkout"]').serializeArray();
+						console.log(values);
+						let country_code = '$keybe_country_code';
+						fetch('https://wrzy3jtldi.execute-api.us-east-1.amazonaws.com/prod/woocommerce/generate-abandoned-cart', {
+							method: 'POST',
+							headers: new Headers({
+								'Content-Type': 'application/json'
+							}),
+							body: JSON.stringify({
+								userData: {
+									name: values.find((item) => item.name === 'billing_first_name')?.value,
+									lastName: values.find((item) => item.name === 'billing_last_name')?.value,
+									phone: country_code + values.find((item) => item.name === 'billing_phone')?.value,
+									email: values.find((item) => item.name === 'billing_email')?.value,
+								},
+								keybeClientData: {
+									companyUUID: '$keybe_company_id',
+									appUUID: '$keybe_app_id',
+									publicKey: '$keybe_api_key',
+									haveSearchPhoneUser: true,
+								},
+							}),
+						});
+					}
+				})
+			</script>
+	<?php endif;
 }
+
 // Post order data to Keybe and unset "abandono el carrito" to "no" on existing user
+// Get settings from settings page
 $options = get_option('keybe_settings');
-// Check if all fields are filled
-if ($options['keybe_app_id'] && $options['keybe_api_key'] && $options['keybe_company_id'] && $options['keybe_country_code']){
+$keybe_app_id = $options['keybe_app_id'];
+$keybe_company_id = $options['keybe_company_id'];
+$keybe_api_key = $options['keybe_api_key'];
+$keybe_country_code = $options['keybe_country_code'];
+if ($keybe_app_id && $keybe_company_id && $keybe_api_key && $keybe_country_code){
 	$active = 1;
 } else {
 	$active = 0;
@@ -83,7 +85,11 @@ add_action('woocommerce_checkout_order_processed', function ($order_id) {
 	if ($order->status != 'failed') {
 		$url = "https://wrzy3jtldi.execute-api.us-east-1.amazonaws.com/prod/woocommerce/sync-abandoned-cart";
 		$options = get_option('keybe_settings');
-		$phone = $options['keybe_country_code'] . $order->get_billing_phone();
+		$keybe_app_id = $options['keybe_app_id'];
+		$keybe_company_id = $options['keybe_company_id'];
+		$keybe_api_key = $options['keybe_api_key'];
+		$keybe_country_code = $options['keybe_country_code'];
+		$phone = $keybe_country_code . $order->get_billing_phone();
 		$response = wp_remote_post(
 			$url,
 			array(
@@ -95,13 +101,12 @@ add_action('woocommerce_checkout_order_processed', function ($order_id) {
 					'email' =>  $order->get_billing_email(),
 					'orde_id' => $order->get_id(),
 					'order_status' => $order->get_status(),
-					'companyUUID' => $options['keybe_company_id'],
-					'appUUID' => $options['keybe_app_id'],
-					'publicKey' => $options['keybe_api_key'],
+					'companyUUID' => $keybe_company_id,
+					'appUUID' => $keybe_app_id,
+					'publicKey' => $keybe_api_key,
 				))
 			)
 		);
 	}
 });
-
 endif;
